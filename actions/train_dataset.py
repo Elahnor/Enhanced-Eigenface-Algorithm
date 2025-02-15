c12f47682e5afc631c9c2dcf05bf0ec6ae0c87ff
Elahnor <rhicasison2021@plm.edu.ph>
2025-02-15 17:26:39 +0800

    Some Changes

--- actions/train_dataset.py
+++ actions/train_dataset.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import cv2
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMessageBox
 
@@ -14,12 +15,10 @@ def train_dataset(self):
         self.train_dataset_btn.setText("Training Dataset")
         os.makedirs("training", exist_ok=True)
 
-        # Determine which dataset to use based on the selected algorithm
         dataset_folder = "Enhanced" if self.enhanced_eigen_algo_radio.isChecked() else "Original"
         labels, faces = get_labels_and_faces(dataset_folder)
 
         try:
-            # Notify user that training has started
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
             msg.setWindowTitle("Training Started")
@@ -28,16 +27,24 @@ def train_dataset(self):
             msg.setWindowIcon(QtGui.QIcon("icon/AppIcon.png"))
             msg.exec_()
 
-            # Train the recognizer
-            self.face_recognizer.train(faces, np.array(labels))
-            
+            if self.enhanced_eigen_algo_radio.isChecked():
+                self.face_recognizer = cv2.face.EigenFaceRecognizer_create()
+                self.lbph_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+                self.face_recognizer.train(faces, np.array(labels))
+                self.lbph_recognizer.train(faces, np.array(labels))
+
+            else:
+                self.face_recognizer = cv2.face.EigenFaceRecognizer_create()
+                self.face_recognizer.train(faces, np.array(labels))
+
             # Save trained dataset
             save_trained_dataset(self)
             self.progress_bar_train.setValue(100)
         
         except Exception as e:
             self.print_custom_error("Unable to Train the Dataset")
-            print(e)
+            print(f"Error during training: {e}")
     else:
         # Reset buttons and progress bar
         self.eigen_algo_radio.setEnabled(True)
@@ -49,7 +56,12 @@ def train_dataset(self):
 def save_trained_dataset(self):
     """Saves the trained dataset."""
     try:
-        self.face_recognizer.save("training/" + self.algorithm.lower() + " trained dataset.yml")
+        if self.enhanced_eigen_algo_radio.isChecked():
+            self.face_recognizer.save("training/enhanced_eigen_trained_dataset.yml")
+            self.lbph_recognizer.save("training/lbph_trained_dataset.yml")
+        else:
+            self.face_recognizer.save("training/eigen_trained_dataset.yml")
+        
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setWindowTitle("Training Completed")
@@ -59,4 +71,4 @@ def save_trained_dataset(self):
         msg.exec_()
     except Exception as e:
         self.print_custom_error("Unable to Save Trained Dataset")
-        print(e)
+        print(f"Error during saving: {e}")
